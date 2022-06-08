d954aa5099ba0dc082091b493e8955e4b1823ec6
mathisveryfun <cwyh311@naver.com>
2022-06-08 12:15:55 +0900

    가우스 공식을 쓸 수도 있구나

--- 2022-06-08 Lv1부족한 금액 계산하기.js
+++ 2022-06-08 Lv1부족한 금액 계산하기.js
@@ -1,15 +1,9 @@
 // 12:07 시작
 
+// 와 가우스 공식을 쓸 수도 있었네..
+
 function solution(price, money, count) {
-  // 필요한 금액 requireMoney 변수를 선언하고 0으로 할당한다.
-  let requireMoney = 0;
-
-  // price, count를 이용해 requireMoney를 계산한다. 
-  for (let i = 1; i <= count; i++) {
-    requireMoney += price * i;    
-  }
-
-  // requireMoney - money 를 리턴한다.
-  return Math.max(requireMoney - money, 0)
+  const tmp = price * count * (count + 1) / 2 - money;
+  return tmp > 0 ? tmp : 0;
 }
 console.log(solution(3, 20, 4))
