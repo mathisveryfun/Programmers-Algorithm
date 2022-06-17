17b906693c75cf551311de477c3d635f7113d63d
mathisveryfun <cwyh311@naver.com>
2022-06-17 22:01:50 +0900

    6월 17일 최종수정

--- 2022-06-12 Lv1 숫자 문자열과영단어No.js
+++ 2022-06-12 Lv1 숫자 문자열과영단어No.js
@@ -1,28 +1,14 @@
 function solution(s) {
-  const tmp = {
-    0: "zero",
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-  }
-  for (let i = 0; i < s.length; i++) {
-    let firstIdx = 0;
-    let lastIdx = 0;
-    if (typeof (s[i]) !== "number") {
-      console.log('test')
-      firstIdx = i;
-      while( typeof (s[i]) === "number") {
-        lastIdx = i;  
-      }
-    }
-    const changeLetter = s.slice(firstIdx, lastIdx)
+  let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  let answer = s;
+
+  for(let i = 0; i < numbers.length; i++) {
+      let arr = answer.split(numbers[i]);
+      console.log(arr)
+      answer = arr.join(i);
+      console.log(answer)
   }
 
+  return Number(answer);
 }
 console.log(solution("one4seveneight"))
