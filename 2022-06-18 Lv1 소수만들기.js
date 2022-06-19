9874382f3f0b092b8f1ea780fe0424447d35382b
mathisveryfun <cwyh311@naver.com>
2022-06-19 19:09:01 +0900

    6월 19일 정답

--- 2022-06-18 Lv1 소수만들기.js
+++ 2022-06-18 Lv1 소수만들기.js
@@ -1,25 +1,26 @@
 function solution(nums) {
-  let count = 0;
-  
-  const isPrime = (num) => {
-      let q = 1;
-      while (q <= Math.sqrt(num)) {
-          if (num % q === 0) {
-              return false;
-          } 
-      q += 1;    
-      }
-      return true;
-  }
-  for (let i = 0; i < nums.length; i++) {
-      for (let j = i; j < nums.length; j++) {
-          for (let k = j; k < nums.length; k++) {
-              if (isPrime(nums[i] + nums[j] + nums[k])) {
-                  count += 1;
-              }                
-          }
-      }
-  }
-  return count;
+    let count = 0;
+    
+    const isPrime = (num) => {
+        let q = 2;
+        while (q <= Math.sqrt(num)) {
+            if (num % q === 0) {
+                return false;
+            } 
+        q += 1;    
+        }
+        return true;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                if (isPrime(nums[i] + nums[j] + nums[k])) {
+                    count += 1;
+                }                
+            }
+        }
+    }
+    return count;
 }
-console.log()
+
+console.log(solution([1, 2, 3, 4 ]))
