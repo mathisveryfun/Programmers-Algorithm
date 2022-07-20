e28b31818331cda8baa9c2d89c18200e3ac9b893
mathisveryfun <cwyh311@naver.com>
2022-07-20 23:53:30 +0900

    test

--- 2022-07-11  Lv2 입국심사.js
+++ 2022-07-11  Lv2 입국심사.js
@@ -1,32 +1,26 @@
 function solution(n, times) {
-  // result를 0으로 선언한다.
-  // 우선 times를 가장 짧은 순으로 다시 재 배열한다. => [7, 10]
-  // times가 빈방인지 아닌지를 구별할 isFull 배열이 필요하다. => [false, false]
-  // n이 0이 될떄 까지 무언가를 반복한다.
-  // isFull배열을 순환하면서 만약 원소값이 false이면은
-  // n을 하나 줄이고, 그자리를 true로 바꾸고,
-  //그 true로 바꾼 원소의 인덱스를 찾아 times에서 값을 찾고 그것을 result에  더함
-  // result를 리턴한다.
-
-  let result = 0;
-  times = times.sort((a, b) => a - b);
-
-  let isFull = new Array(times.length).fill(0);
-
-  while (n > 0) {
-    isFull = isFull.map((el, idx, arr) => {
-      if (el === 0) {
-        n = n - 1;
-        return times[idx];
-      } else {
-        return el;
+  times.sort((a,b) => a-b); 
+  let left = 1;
+  let right = n * times[times.length -1];
+  let answer = right; // 최대값.
+  while(left <= right){
+      let mid = Math.floor((left + right) / 2);
+      let count = 0
+      times.forEach(value => {
+          count += Math.floor(mid / value); // 한 사람당 몇명 할 수 있는지
+          console.log("미드", mid, value, count)
+          if(count >= n) {
+              answer = Math.min(mid, answer); // 최솟값
+              return;
+          };
+      });
+      if (count >= n) { 
+          right = mid - 1;
       }
-    });
-    result = result + 1;
-    isFull = isFull.map((el) => {
-      return el - 1;
-    });
+      else {
+          left = mid + 1;
+      }
   }
-  return result
+  return answer;
 }
-console.log(solution(6, [7, 10]));
+console.log(solution(5, [7, 10]))
